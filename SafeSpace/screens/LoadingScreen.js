57c603ef3165a55308b07adf67b77cb7ebc78a57
KingWazzack <paulkros@hotmail.com>
2023-09-18 12:31:58 +0200

    BP5OT01-29 Created ScoreScreen
    Created placeholder LoadingScreen.js and ScoreScreen.js

---
+++ SafeSpace/screens/LoadingScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import React, { Component } from "react";
+
+export class LoadingScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Loading</Text>
+      </View>
+    );
+  }
+}
+
+export default LoadingScreen;
